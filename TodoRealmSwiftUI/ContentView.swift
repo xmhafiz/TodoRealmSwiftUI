d1b09a5ba76f6c02820db29f23c014009b693368
Mohd Hafiz <xmhafiz@gmail.com>
2021-09-25 17:35:08 +0800

    init project

---
+++ TodoRealmSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TodoRealmSwiftUI
+//
+//  Created by Hafiz on 25/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoRealmSwiftUI/TodoRealmSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoRealmSwiftUIApp.swift
+//  TodoRealmSwiftUI
+//
+//  Created by Hafiz on 25/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoRealmSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
